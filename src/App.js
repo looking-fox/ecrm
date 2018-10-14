956b0a6f5ba0829eb579172afa99fec0feaa891a
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-10-14 14:10:55 -0600

    Setting up Bootstrap Grid.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './reset.css'
-import './App.css'
+
 import Home from './components/Home/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid background">
-        <Home/>
-      </div>
+      <Home/>
     )
   }
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import './Home.css'
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        Hello World
+      <div className="container-fluid background">
+
+        <div className="col w-100 imga">
+            <div className="row">
+            hello
+            </div>
+        </div>
+    
       </div>
     )
   }
