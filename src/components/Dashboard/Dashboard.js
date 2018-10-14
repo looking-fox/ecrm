4c2bc1cc0a5385a13dbbc755821cc40f3fe58c0b
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-10-14 14:50:30 -0600

    Dashboard Component added.

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Dashboard.css'
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        Dashboard Component
+      </div>
+    )
+  }
+}
