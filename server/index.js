e558f38e4552c404433e305fb68ef95c4b57eb71
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-10-17 02:41:50 -0700

    Auth and Proxy successful.

--- server/index.js
+++ server/index.js
@@ -14,3 +14,42 @@ app.use(session({
     saveUninitialized: false,
     resave: false
 }))
+
+
+
+//========Auth0==========//
+
+app.get('/auth/callback', async (req, res) => {
+    const {PROTOCOL} = process.env
+        const payload = {
+            client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+            client_secret: process.env.AUTH0_CLIENT_SECRET,
+            code: req.query.code,
+            grant_type: 'authorization_code',
+            redirect_uri: `${PROTOCOL}://${req.headers.host}/auth/callback`
+        };
+    //----get token---//
+    
+        let receiveToken = await axios.post(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/oauth/token`, payload)
+    
+    //----exchange token---//
+    
+    let receiveUser = await axios.get(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/userinfo?access_token=${receiveToken.data.access_token}`)
+    
+    //-----user data-----//
+    req.session.user = receiveUser.data;
+    req.session.user.id = sessionId;
+    sessionId++
+    res.status(200).send(req.session.user)
+
+})
+
+//========Auth0==========//
+
+
+
+const SERVER_PORT = process.env.SERVER_PORT || 3050
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is listening on ${SERVER_PORT}`)
+})
+
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 import './Home.css'
 import head from '../../assets/head.jpg'
 import Features from '../Features/Features'
 
 
 export default class Home extends Component {
+  constructor(){
+    super()
+    
+  }
+
+  login = () => {
+
+    let {REACT_APP_AUTH0_DOMAIN,
+      REACT_APP_AUTH0_CLIENT_ID
+    } = process.env;
+  
+    let url = `${encodeURIComponent(window.location.origin)}/auth/callback`
+  
+    window.location = `https://${REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_url=${url}&response_type=code`
+  
+  }
+
+
   render() {
     return (
       <div className="app">
@@ -16,7 +35,7 @@ export default class Home extends Component {
              <li>FEATURES</li>
              <li>PRICING</li>
              <Link to="/contact"><li>CONTACT</li></Link>
-             <button type="button" className="btn btn-sm btn-outline-dark">LOG IN</button>
+             <button type="button" className="btn btn-sm btn-outline-dark" onClick={() => this.login()}>LOG IN</button>
            </nav>
         </div>
 
