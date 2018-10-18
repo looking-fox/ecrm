3383e996278bee8e66e5d20da3c9eb7878a4ca09
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-10-18 00:23:30 -0700

    default client item created with styling.

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -5,7 +5,42 @@ export default class Clients extends Component {
   render() {
     return (
       <div className="clientdashboard">
-        Clients
+
+        <div className="client">
+
+          <div className="name item">
+          <p>John Smith</p>
+          </div>
+
+          <div className="email item">
+          <p>jsmith@gmail.com</p>
+          </div>
+
+          <div className="package item">
+          <span className="bubble">Elopement</span>
+          </div>
+
+          <div className="date item">
+          <p>10 / 05 / 2018</p>
+          </div>
+
+          <div className="location item">
+          <p>Montana</p>
+          </div>
+
+          <div className="total item">
+          <p>$2,500</p>
+          </div>
+
+          <div className="notes">
+          <p><i className="fas fa-comment"/></p>
+          </div>
+
+        </div>
+
+
+        
+
       </div>
     )
   }
--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -5,17 +5,17 @@ export default class Features extends Component {
   render() {
     return (
       <div className="features">
-        <div className="item">
+        <div className="featureitem">
         <i className="fas fa-file-alt"></i>
           <p>CUSTOM FEEDBACK </p>
         </div>
 
-        <div className="item">
+        <div className="featureitem">
         <i className="fas fa-users"></i>
           <p>MANAGE CLIENTS </p>
         </div>
 
-        <div className="item">
+        <div className="featureitem">
         <i className="fas fa-list-ul"></i>
           <p>TO DO LIST </p>
         </div>
