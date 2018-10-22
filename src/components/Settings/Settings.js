38087dfba5c66ef32944deb77cd0d8c5f360244a
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-10-22 10:54:56 -0700

    Working on Settings navbar.

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import './Settings.css'
+import {Link} from 'react-router-dom'
 
 export default class Settings extends Component {
   render() {
     return (
       <div className="settingsdashboard">
-        Settings
+
+      <navbar>
+        <p className="menuitem">General</p>
+        <p className="menuitem">Subscription</p>
+        <p className="menuitem">Sessions</p>
+      </navbar>
+    
       </div>
     )
   }
