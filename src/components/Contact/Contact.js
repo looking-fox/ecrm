5fdd5238d3b8c228f01c3f883e48e1d90422d16a
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-10-30 22:24:38 -0500

    Starting work on contact.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 import './Contact.css'
-
+import {Link} from 'react-router-dom'
 export default class Contact extends Component {
   render() {
     return (
       <div className="contact">
-        Contact
+      
+      {/* Warning: Will need to make the navbar a component to avoid duplicating code. */}
+
+        <div className="header">
+           <h1 className="logo">R/H</h1>
+           <nav>
+             <li>FEATURES</li>
+             <li>PRICING</li>
+             <Link to="/contact"><li>CONTACT</li></Link>
+             <button type="button" className="btn btn-sm btn-outline-dark" onClick={() => this.login()}>LOG IN</button>
+           </nav>
+        </div>
+
+
       </div>
     )
   }
