45fd52cdc6158ace7c794c7910490e2213d58207
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-11-08 21:38:40 -0600

    uri test.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,6 +26,8 @@ export default class Home extends Component {
     } = process.env;
   
     let url = `${encodeURIComponent(window.location.origin)}/auth/callback`
+
+    console.log('url', url)
   
     window.location = `https://${REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_url=${url}&response_type=code`
   
