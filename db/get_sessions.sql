b88f10a10096c1440e3d77a334129df8304a1fd6
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-12-24 16:52:35 -0600

    Sessions query fixed.

--- db/get_sessions.sql
+++ db/get_sessions.sql
@@ -1,18 +1,21 @@
-with exports as (
+with a as (
     select session_id, jsonb_array_elements_text(actions)::jsonb as doc from sessions
+),
+b as (
+    select session_id, jsonb_agg(doc) as actions from a
+    group by session_id
 )
-select exports.session_id, sessions.user_id, sessions.session_name, 
+
+select b.session_id, sessions.user_id, sessions.session_name, 
 sessions.session_color, sessions.session_price,
-jsonb_agg(doc) as actions, sessions.template from exports
+b.actions, sessions.template from b
 
 left join sessions on
-sessions.session_id = exports.session_id
+sessions.session_id = b.session_id
 
 where sessions.user_id = $1
 and sessions.template = true
 
-group by exports.session_id, sessions.user_id, 
+group by b.session_id, sessions.user_id, b.actions,
 sessions.session_name, sessions.session_color, sessions.session_price, sessions.template
 order by session_id;
-
-
