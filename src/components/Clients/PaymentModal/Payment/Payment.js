bbbb68c1abc727c3fc398d978bf9b52aaec2a803
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-01-12 16:43:14 -0600

    payment logs for filter check.

--- src/components/Clients/PaymentModal/Payment/Payment.js
+++ src/components/Clients/PaymentModal/Payment/Payment.js
@@ -1,90 +1,92 @@
 import React, { Component } from 'react'
 import DatePicker from 'react-date-picker'
-import {convertRawMoney, convertToRawMoney} from '../../../../Main/MainLogic'
+import { convertRawMoney, convertToRawMoney } from '../../../../Main/MainLogic'
 
 export default class Payment extends Component {
-  constructor(){
-      super()
-      this.state = {
-          amount: '', 
-          description: '',
-          date: new Date()
-      }
-  }
-
-  componentDidMount(){
-      if(this.props.payment){
-      const {amount, description, date} = this.props.payment
-      var filterDate = new Date(date)
-      var filterAmount = convertRawMoney(amount)
-      this.setState({ amount: filterAmount, description, date: filterDate })
-      }
-  }
-
-  //Convert string input to raw integer on change. Set state to string value with commas/$ symbol. But pass up integer to store.
-  updateAmount = () => { 
-    if(this.state.amount){
-        const {amount, description, date} = this.state
-        let id = this.props.payment.payment_id
-        let intAmount = convertToRawMoney(amount)
-        let filterAmount = convertRawMoney(intAmount)
-
-        this.setState({amount: filterAmount}, () => {
-            let newInfo = {amount: intAmount, description, date}
-            this.props.updatePayment(id, newInfo, this.props.index)
-        })   
+    constructor() {
+        super()
+        this.state = {
+            amount: '',
+            description: '',
+            date: new Date()
+        }
     }
-  }
 
-  updateDate = (date, id) => {
-      this.setState({ date })
-      const {amount, description} = this.state
-      let intAmount = convertToRawMoney(amount)
-      let newInfo = {amount: intAmount, date, description}
-      this.props.updatePayment(id, newInfo)
-  }
+    componentDidMount() {
+        if (this.props.payment) {
+            const { amount, description, date } = this.props.payment
+            console.log('raw date: ', date)
+            var filterDate = new Date(date)
+            console.log('filter: ', filterDate)
+            var filterAmount = convertRawMoney(amount)
+            this.setState({ amount: filterAmount, description, date: filterDate })
+        }
+    }
+
+    //Convert string input to raw integer on change. Set state to string value with commas/$ symbol. But pass up integer to store.
+    updateAmount = () => {
+        if (this.state.amount) {
+            const { amount, description, date } = this.state
+            let id = this.props.payment.payment_id
+            let intAmount = convertToRawMoney(amount)
+            let filterAmount = convertRawMoney(intAmount)
+
+            this.setState({ amount: filterAmount }, () => {
+                let newInfo = { amount: intAmount, description, date }
+                this.props.updatePayment(id, newInfo, this.props.index)
+            })
+        }
+    }
+
+    updateDate = (date, id) => {
+        this.setState({ date })
+        const { amount, description } = this.state
+        let intAmount = convertToRawMoney(amount)
+        let newInfo = { amount: intAmount, date, description }
+        this.props.updatePayment(id, newInfo)
+    }
 
 
-  render() {
-      const {payment, index} = this.props
-      const {amount, description, date} = this.state
-      let filterAmount = convertToRawMoney(amount)
-      let newInfo = {amount: filterAmount, description, date}
-      let id = payment.payment_id
+    render() {
+        const { payment, index } = this.props
+        const { amount, description, date } = this.state
+        let filterAmount = convertToRawMoney(amount)
+        let newInfo = { amount: filterAmount, description, date }
+        let id = payment.payment_id
 
-    return (
-        <div className="row">
+        return (
+            <div className="row">
 
-            <div className="pay pay-amount">
+                <div className="pay pay-amount">
 
-                <input 
-                className="row-input amount-input"
-                placeholder="$50"
-                onChange={e => this.setState({amount: e.target.value})}
-                onBlur={() => this.updateAmount() }
-                value={this.state.amount}/>
+                    <input
+                        className="row-input amount-input"
+                        placeholder="$50"
+                        onChange={e => this.setState({ amount: e.target.value })}
+                        onBlur={() => this.updateAmount()}
+                        value={this.state.amount} />
+
+                </div>
+
+                <div className="pay pay-date">
+                    <DatePicker
+                        onChange={e => this.updateDate(e, id)}
+                        value={this.state.date} />
+                </div>
+
+                <div className="pay pay-description">
+
+                    <input
+                        className="row-input"
+                        placeholder="Deposit"
+                        value={this.state.description}
+                        onChange={e => this.setState({ description: e.target.value })}
+                        onBlur={e => this.props.updatePayment(id, newInfo)} />
+                    <i className="far fa-trash-alt row-delete"
+                        onClick={() => this.props.verifyDelete(index, payment)} />
+                </div>
 
             </div>
-
-            <div className="pay pay-date">
-                <DatePicker
-                onChange={e => this.updateDate(e, id)}
-                value={this.state.date}/>
-            </div>
-
-            <div className="pay pay-description">
-
-                <input 
-                className="row-input"
-                placeholder="Deposit"
-                value={this.state.description}
-                onChange={e => this.setState({description: e.target.value})}
-                onBlur={e => this.props.updatePayment(id, newInfo) }/>
-                <i className="far fa-trash-alt row-delete"
-                onClick={() => this.props.verifyDelete(index, payment)}/>
-            </div>
-
-        </div>
-    )
-  }
+        )
+    }
 }
