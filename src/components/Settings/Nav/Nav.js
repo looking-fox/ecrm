a5bdf9716256e34c9239995f9cab6367bb79b2e5
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-02-16 22:53:42 -0600

    fix: major styling issue resolved.

--- src/components/Settings/Nav/Nav.js
+++ src/components/Settings/Nav/Nav.js
@@ -2,18 +2,18 @@ import React from 'react'
 import './Nav.css'
 import { NavLink } from 'react-router-dom'
 
-export default function Nav() { 
+export default function Nav() {
   return (
     <div className="menu-bar align-center">
-  
-        <NavLink to="/tools/templates">
-        <li className="nav-item center">Templates</li>
-        </NavLink>
 
-        <NavLink to="/tools/finances">
+      <NavLink to="/tools/templates">
+        <li className="nav-item center">Templates</li>
+      </NavLink>
+
+      <NavLink to="/tools/finances">
         <li className="nav-item center">Finances</li>
-        </NavLink>
-   
-      </div>
+      </NavLink>
+
+    </div>
   )
 }
